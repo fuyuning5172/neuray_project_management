6356ce4e7d9cadafe4aa175ce4c20d5c6ef8c2cd
unknown <1152085232@qq.com>
2019-08-30 09:19:34 +0800

    gengxin

---
+++ master/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from master.views import *
+
+app_name = 'app'
+
+urlpatterns = [
+	path('index/', index, name='index'),
+    path('upload_file/',upload_file,name = 'upload_file'),
+    path('login/',login,name='login'),
+    # path('main/',main,name='main')
+    ]
--- neuray_project_management/urls.py
+++ neuray_project_management/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from neuray_project_management import view
+from django.urls import path, include
+from master.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('master/',include('master.urls',namespace='master')),
+    path('',index)
 ]
